910612c26a492be3a58a047caf0a7988744b9d6f
Kaviraj Pandey <kaviraj.pandey11@gmail.com>
2022-01-24 22:44:25 +0530

    My Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:assignment1/pin_code_field.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
@@ -20,7 +21,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyStatelessWidget extends StatelessWidget {
-  TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -31,212 +31,18 @@ class MyStatelessWidget extends StatelessWidget {
             context: context,
             isScrollControlled: true,
             clipBehavior: Clip.antiAliasWithSaveLayer,
+            backgroundColor: Colors.transparent,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.vertical(
                 top: Radius.circular(25),
               ),
             ),
             builder: (BuildContext context) {
-              return Padding(
-                padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).viewInsets.bottom),
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.55,
-                  padding:
-                      EdgeInsets.only(left: 16, top: 20, right: 16, bottom: 16),
-                  alignment: Alignment.center,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      //Parents only section column 1
-                      Container(
-                        padding: EdgeInsets.all(4.0),
-                        margin: EdgeInsets.only(top: 26),
-                        child: AutoSizeText(
-                          'Parents Only',
-                          style: TextStyle(
-                            fontSize: 26.0,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0XFF2DAD9D),
-                            fontFamily: 'Poppins',
-                          ),
-                          textAlign: TextAlign.center,
-                          minFontSize: 18,
-                        ),
-                      ),
-
-                      //Please only section column 2
-                      Container(
-                        margin: EdgeInsets.only(top: 4, bottom: 4),
-                        child: AutoSizeText(
-                          'To access,\n'
-                          'Please enter the below numbers',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w400,
-                            fontFamily: 'Poppins',
-                            color: Color(0XFF000000),
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-
-                      //Yellow OTP section Column 3
-                      Container(
-                        width: MediaQuery.of(context).size.height * 0.40,
-                        height: MediaQuery.of(context).size.height * 0.06,
-                        margin: EdgeInsets.only(top: 10, bottom: 12),
-                        padding: EdgeInsets.all(1),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Color(0XFFFFDC00),
-                          border: Border.all(
-                            color: Colors.white70,
-                          ),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(15),
-                          ),
-                        ),
-                        child: AutoSizeText(
-                          addString(),
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0XFF000000),
-                            backgroundColor: Color(0XFFFFDC00),
-                            //wordSpacing: 1,
-                          ),
-                          textAlign: TextAlign.center,
-                          minFontSize: 16,
-                          maxLines: 2,
-                        ),
-                      ),
-
-                      //Pin code field column section 4
-                      Container(
-                        padding: EdgeInsets.all(2),
-                        alignment: Alignment.center,
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        height: MediaQuery.of(context).size.height * 0.075,
-                        margin: EdgeInsets.only(top: 4, bottom: 8),
-                        child: PinCodeTextField(
-                          showCursor: true,
-                          cursorColor: Colors.blue,
-                          cursorWidth: 1,
-                          cursorHeight: 16,
-                          appContext: context,
-                          autoFocus: true,
-                          length: 4,
-                          keyboardType: TextInputType.phone,
-                          obscureText: false,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          scrollPadding: EdgeInsets.only(),
-                          textStyle: TextStyle(
-                            fontSize: 25.0,
-                          ),
-                          pinTheme: PinTheme(
-                            shape: PinCodeFieldShape.box,
-                            borderRadius: BorderRadius.circular(6),
-                            activeColor: Color.fromRGBO(19, 83, 203, 1),
-                            selectedFillColor: Color.fromRGBO(19, 83, 203, 1),
-                            selectedColor: Color.fromRGBO(19, 83, 203, 1),
-                            //activeFillColor: Color.fromRGBO(19, 83, 203, 1),
-                            borderWidth: 1,
-                            errorBorderColor: Colors.redAccent,
-                            inactiveColor: Color.fromRGBO(19, 83, 203, 1),
-                            fieldHeight:
-                                MediaQuery.of(context).size.height * 0.06,
-                            fieldWidth:
-                                MediaQuery.of(context).size.height * 0.07,
-                          ),
-                          //animationDuration: Duration(milliseconds: 300),
-                          //enableActiveFill: true,
-                          onChanged: (value) {},
-                          onCompleted: (v) {
-                            print("Completed");
-                          },
-                          beforeTextPaste: (text) {
-                            print("Allowing to paste $text");
-                            //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
-                            //but you can show anything you want here, like your pop up saying wrong paste format or etc
-                            return true;
-                          },
-                        ),
-                      ),
-
-                      //Submit button Column 5
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.055,
-                        width: MediaQuery.of(context).size.height * 0.28,
-                        padding: EdgeInsets.only(
-                            left: 3, top: 1, right: 3, bottom: 1),
-                        margin: EdgeInsets.only(top: 4, bottom: 12),
-                        child: GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Color(0XFF1353CB),
-                              border: Border.all(
-                                color: Color(0XFF1353CB),
-                              ),
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                AutoSizeText(
-                                  'SUBMIT',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Poppins',
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                  minFontSize: 12,
-                                  maxLines: 2,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
+              return CustomPinField();
             },
           );
         },
       ),
     );
-  }
-
-  //Function to return string in OTP field
-  String addString() {
-    String newStr = '';
-    String str;
-    var currentNum;
-    final Map<int, String> someMap = {
-      0: 'Zero',
-      1: 'One',
-      2: 'Two',
-      3: 'Three',
-      4: 'Four',
-      5: 'Five',
-      6: 'Six',
-      7: 'Seven',
-      8: 'Eight',
-      9: 'Nine'
-    };
-    var ran = new Random();
-    for (var i = 0; i < 4; i++) {
-      currentNum = ran.nextInt(10);
-      str = someMap[currentNum].toString();
-      newStr = newStr + ' ' + str;
-    }
-    return newStr;
   }
 }
--- lib/pin_code_field.dart
+++ lib/pin_code_field.dart
@@ -1,1 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'dart:math';
 
+class CustomPinField extends StatefulWidget {
+  const CustomPinField({Key? key}) : super(key: key);
+
+  @override
+  _CustomPinFieldState createState() => _CustomPinFieldState();
+}
+
+class _CustomPinFieldState extends State<CustomPinField> {
+  String numStr = '';
+  String currentText = '';
+  bool hasError = false;
+  TextEditingController textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(25),
+        ),
+      ),
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.50,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            //Parents only section column 1
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              child: AutoSizeText(
+                'Parents Only',
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0XFF2DAD9D),
+                  fontFamily: 'Poppins',
+                ),
+                textAlign: TextAlign.center,
+                minFontSize: 18,
+              ),
+            ),
+
+            //Please only section column 2
+            Container(
+              margin: EdgeInsets.only(top: 12, bottom: 12),
+              child: AutoSizeText(
+                'To access,\n'
+                'Please enter the below numbers',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Poppins',
+                  color: Color(0XFF000000),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+            //Yellow OTP section Column 3
+            Container(
+              width: MediaQuery.of(context).size.width * 0.70,
+              height: MediaQuery.of(context).size.height * 0.5 * 0.15,
+              margin: EdgeInsets.only(bottom: 12),
+              padding: EdgeInsets.all(1),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Color(0XFFFFDC00),
+                border: Border.all(
+                  color: Colors.white70,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+              child: AutoSizeText(
+                addString(),
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0XFF000000),
+                  //wordSpacing: 1,
+                ),
+                textAlign: TextAlign.center,
+                minFontSize: 18,
+                maxLines: 1,
+              ),
+            ),
+
+            //Pin code field column section 4
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.5 * 0.15,
+              margin: EdgeInsets.only(bottom: 26),
+              child: PinCodeTextField(
+                showCursor: true,
+                cursorColor: Colors.blue,
+                cursorWidth: 1,
+                appContext: context,
+                autoFocus: true,
+                length: 4,
+                keyboardType: TextInputType.phone,
+                obscureText: false,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                scrollPadding: EdgeInsets.only(),
+                textStyle: TextStyle(
+                  fontSize: 20.0,
+                ),
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(6),
+                  activeColor: hasError ? Color(0XFF1353CB) : Colors.redAccent,
+                  selectedFillColor: Color(0XFF1353CB),
+                  selectedColor: Color(0XFF1353CB),
+                  //activeFillColor: Color.fromRGBO(19, 83, 203, 1),
+                  borderWidth: 1,
+                  errorBorderColor: Colors.redAccent,
+                  inactiveColor: Color(0XFF1353CB),
+                  fieldHeight: MediaQuery.of(context).size.height * 0.5 * 0.15,
+                  fieldWidth: 60,
+                ),
+                //animationDuration: Duration(milliseconds: 300),
+                //enableActiveFill: true,
+                onChanged: (value) {
+                  if (value == numStr) {
+                    setState(() {
+                      hasError = true;
+                    });
+                  } else {
+                    hasError = false;
+                  }
+                },
+                onCompleted: (v) {
+                  print("Completed");
+                },
+              ),
+            ),
+            //Submit button Column 5
+            Container(
+              height: 50,
+              width: 210,
+              margin: EdgeInsets.only(bottom: 14),
+              child: GestureDetector(
+                onTap: () {
+                  if (hasError == true) {
+                    print('Sucess!');
+                  } else {
+                    print('Try Again!');
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0XFF1353CB),
+                    border: Border.all(
+                      color: Color(0XFF1353CB),
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: AutoSizeText(
+                    'SUBMIT',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                    minFontSize: 8,
+                    maxLines: 1,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //Function to return string in OTP field
+  String addString() {
+    String newStr = '';
+    String str;
+    var currentNum;
+    final Map<int, String> someMap = {
+      0: 'Zero',
+      1: 'One',
+      2: 'Two',
+      3: 'Three',
+      4: 'Four',
+      5: 'Five',
+      6: 'Six',
+      7: 'Seven',
+      8: 'Eight',
+      9: 'Nine'
+    };
+    var ran = new Random();
+    for (var i = 0; i < 4; i++) {
+      currentNum = ran.nextInt(10);
+      numStr = numStr + currentNum.toString();
+      str = someMap[currentNum].toString();
+      newStr = newStr + '' + str;
+    }
+    return newStr;
+  }
+}
